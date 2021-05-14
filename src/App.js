4f047e2fc2c1ed484cef7d7f507b6459569ac7c1
jallend1 <jallend1@gmail.com>
2021-05-14 16:17:36 -0700

    Scaffolds out NavBar and Home components. Installs Material-UI and Roboto fonto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import NavBar from './Components/NavBar';
+import Home from './Components/Home';
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <Home />
     </div>
   );
 }
---
+++ src/Components/NavBar.js
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return (
+        <nav>
+            Navbar!
+        </nav>
+    )
+}
+
+export default NavBar;
