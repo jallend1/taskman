48c02a4ef39cc852daa6be2c8d7808bea4070a97
jallend1 <jallend1@gmail.com>
2021-05-15 12:15:22 -0700

    Applies basic styling to completed items

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,13 +1,22 @@
-import {Checkbox, List, ListItem, ListItemIcon, ListItemText, Typography } from '@material-ui/core';
+import {Checkbox, List, ListItem, ListItemIcon, ListItemText, Typography, makeStyles } from '@material-ui/core';
 const Project = ({project, completeTask}) => {
+    const useStyles = makeStyles({
+        completed: {
+            textDecoration: 'line-through',
+            opacity: .4
+        }
+    })
+
+    const classes = useStyles();
+
     const renderProjects = () => {
         return project.taskList.map((task, index) => {
             return (
                 <ListItem key={task.action} >
                     <ListItemIcon>
-                        <Checkbox checked={task.isComplete} onClick={() => completeTask(project.id, index)}/>
+                        <Checkbox checked={task.isComplete} onClick={() => completeTask(project.id, index)} />
                     </ListItemIcon>
-                    <ListItemText primary={task.action} />
+                    <ListItemText primary={task.action} className={task.isComplete ? classes.completed : null}/>
                 </ListItem>
 
             )})
