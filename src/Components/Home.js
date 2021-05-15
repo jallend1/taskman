c992228d9e58bdef0a2f170f8609c3c5c65c0ee1
jallend1 <jallend1@gmail.com>
2021-05-15 12:02:04 -0700

    Resolves bug where tasks were not getting marked complete

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,7 @@ import {useState} from 'react';
 import Project from './Project';
 // import About from './About';
 const Home = () => {
-    const [project, setProject ] = useState({
+    const sampleProject = {
         title: 'My First Project!',
         id: 5000,
         taskList: [
@@ -10,21 +10,24 @@ const Home = () => {
             {action: 'Do laundry', isComplete: false},
             {action: 'Union meeting', isComplete: false}
         ]
-    })
-
-    const completeTask = (projectID, index) => {
-        // Passing in task so we can navigate to correct project once multiple project functionality rolled out
-        const projectCopy = project;
-        projectCopy.taskList[index].isComplete = true;
-        setProject(projectCopy)
     }
 
+    const [project, setProject ] = useState(sampleProject)
+
+
+    const completeTask = (projectID, index) => {
+        // Passing in project ID so we can navigate to correct project once multiple project functionality rolled out
+        // Creates deep copy of current project
+        const projectCopy = JSON.parse(JSON.stringify(project));
+        projectCopy.taskList[index].isComplete = !projectCopy.taskList[index].isComplete;
+        setProject(projectCopy)
+    }
 
     return (
         <>
             <h1>Home!</h1>
             {/* <About /> */}
-            <Project key={project.id} project={project} completeTask = {completeTask}/>
+            <Project key={project.id} project={project} completeTask={completeTask}/>
         </>
     )
 }
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,19 +1,22 @@
 import {Checkbox, List, ListItem, ListItemIcon, ListItemText, Typography } from '@material-ui/core';
 const Project = ({project, completeTask}) => {
+    const renderProjects = () => {
+        return project.taskList.map((task, index) => {
+            return (
+                <ListItem key={task.action} >
+                    <ListItemIcon>
+                        <Checkbox checked={task.isComplete} onClick={() => completeTask(project.id, index)}/>
+                    </ListItemIcon>
+                    <ListItemText primary={task.action} />
+                </ListItem>
+
+            )})
+    }
     return (
         <>
             <Typography variant="h2">{project.title}</Typography>
             <List>
-                {project.taskList.map((task, index) => {
-                return (
-                    <ListItem key={task.action} >
-                        <ListItemIcon>
-                            <Checkbox checked={task.isComplete} onClick={() => completeTask(project.id, index)}/>
-                        </ListItemIcon>
-                        <ListItemText primary={task.action} />
-                    </ListItem>
-
-                )})}
+                {project ? renderProjects() : 'Add a project'}
             </List>
         </>
         
