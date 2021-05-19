2847e2b42ab6039c58a6809c820ecd17e781e006
jallend1 <jallend1@gmail.com>
2021-05-18 19:19:52 -0700

    Adds basic styling

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,11 @@
 import { Typography } from "@material-ui/core";
 
 const Footer = () => {
-  return <Typography variant="h6">Made by Jason</Typography>;
+  return (
+    <Typography variant="h6" align="center" className="footer">
+      Made by Jason
+    </Typography>
+  );
 };
 
 export default Footer;
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,4 +1,5 @@
 import {
+  Box,
   CardActionArea,
   Checkbox,
   List,
@@ -11,27 +12,27 @@ import {
   CardContent,
   TextField,
   ListItemSecondaryAction,
-  IconButton
-} from '@material-ui/core';
-import { DeleteOutlined } from '@material-ui/icons';
-import { useEffect, useState } from 'react';
+  IconButton,
+} from "@material-ui/core";
+import { DeleteOutlined } from "@material-ui/icons";
+import { useEffect, useState } from "react";
 
 const Project = ({ project, addTask, completeTask, deleteTask }) => {
-  const [newAction, setNewAction] = useState('');
+  const [newAction, setNewAction] = useState("");
   const useStyles = makeStyles({
     root: {
       width: 400,
-      margin: '2em auto'
+      margin: "2em auto",
     },
     completed: {
-      textDecoration: 'line-through',
-      opacity: 0.4
-    }
+      textDecoration: "line-through",
+      opacity: 0.4,
+    },
   });
 
   const classes = useStyles();
   //   When Project is updated, resets the action state to empty string
-  useEffect(() => setNewAction(''), [project]);
+  useEffect(() => setNewAction(""), [project]);
 
   const renderTasks = () => {
     return project.taskList.map((task, index) => {
@@ -60,25 +61,26 @@ const Project = ({ project, addTask, completeTask, deleteTask }) => {
       );
     });
   };
-  
+
   return (
     <>
-      <Card className={classes.root}>
+      <Card className={classes.root} boxShadow={3}>
         <CardHeader
           title={project.title}
           subheader={project.createdAt.toString()}
         />
         <CardContent>
-          <List>{project ? renderTasks() : 'Add a project'}</List>
+          <List>{project ? renderTasks() : "Add a project"}</List>
         </CardContent>
         <CardActionArea>
           <form onSubmit={(e) => addTask(e, project.id, newAction)}>
             <TextField
               label="Add next action"
-              variant="outlined"
-              color="secondary"
               onChange={(e) => setNewAction(e.target.value)}
               fullWidth
+              autoFocus
+              variant="outlined"
+              color="primary"
             />
           </form>
         </CardActionArea>
