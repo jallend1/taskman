e2767d518b89b3965c11af101682032f3a9cfe3b
jallend1 <jallend1@gmail.com>
2021-05-29 13:32:28 -0700

    Adds alt text to version one screenshot

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,7 +7,7 @@ const About = () => {
         <Typography variant="h2">About</Typography>
         <Typography variant="body1">Just a basic little tasklist app.</Typography>
         <Typography variant="h5">Explore previous versions: </Typography>
-        <img src="./images/version1.jpg" />
+        <img src="./images/version1.jpg" alt="Layout of version 1"/>
 
         {/* <V1 /> */}
         </>
