41cf04d30079f8a27bc3fbfa27721ae0e654ddd1
jallend1 <jallend1@gmail.com>
2021-06-04 15:23:03 -0700

    Begins importing auth context into login page

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react';
 import { Button } from "@material-ui/core";
+import { AuthContext } from '../Contexts/AuthContext'
 
 const Login = () => {
+  const { user } = useContext(AuthContext)
   return (
     <>
       <Button variant="secondary">Login</Button>
