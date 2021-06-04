4d228457ed9840b47739c5df1b7c789f25312cfd
jallend1 <jallend1@gmail.com>
2021-06-03 20:27:45 -0700

    Adds beginnings of login page

---
+++ src/Components/login.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@material-ui/core";
+
+const Login = () => {
+  return (
+    <>
+      <Button variant="secondary">Login</Button>
+    </>
+  );
+};
+
+export default Login;
