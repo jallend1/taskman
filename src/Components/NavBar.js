ba67fc66c1219daa0bc80abff78981102d13d98e
jallend1 <jallend1@gmail.com>
2021-06-09 16:19:06 -0700

    Removes styling for profile image and instead uses Avatar component from MaterialUI

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,12 @@
 import { useContext } from 'react';
 import { Link as RRDLink } from 'react-router-dom';
-import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  Avatar,
+  IconButton,
+  Toolbar,
+  Typography
+} from '@material-ui/core';
 import { makeStyles, Button } from '@material-ui/core';
 import { AuthContext } from '../Contexts/AuthContext';
 import { AccountCircle } from '@material-ui/icons';
@@ -13,24 +19,22 @@ const NavBar = () => {
     },
     leftBar: {
       flexBasis: '90%'
-    },
-    userPhoto: { 
-      borderRadius: "50%",
-      height: 50
     }
   }));
 
   const loggedIn = () => {
-    console.log(user)
     return (
       <>
-
         <IconButton
           label="Current user account info"
           component={RRDLink}
           to="/profile"
         >
-        {user.photoURL ? <img src={user.photoURL} alt="profile icon" className={classes.userPhoto}/>: <AccountCircle />}
+          {user.photoURL ? (
+            <Avatar alt={user.displayName || null} src={user.photoURL} />
+          ) : (
+            <AccountCircle />
+          )}
         </IconButton>
         <Button onClick={logout}>Logout</Button>
       </>
