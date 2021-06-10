5bf01eec662a1c24c50a90c22516c15ea4d9004d
jallend1 <jallend1@gmail.com>
2021-06-10 15:57:46 -0700

    Begins styling login page

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -1,20 +1,42 @@
 import { useContext } from 'react';
-import { Button } from '@material-ui/core';
+import { makeStyles, Button, Paper, Typography } from '@material-ui/core';
 import { AuthContext } from '../Contexts/AuthContext';
 
 const Login = () => {
   const { user, loginWithGoogle, logout } = useContext(AuthContext);
+  const useStyles = makeStyles({
+    loginBox: {
+      maxWidth: "600px",
+      margin: "2em auto",
+      padding: "2em"
+    },
+    title: {
+      textAlign: "center"
+    }
+  })
+
+  const classes=useStyles();
+  
+  const loginPage = () => {
+    return (
+      <>
+      <Paper className={classes.loginBox}>
+      <Typography variant="h4" className={classes.title}>Log into TaskMan</Typography>
+        <Button color="primary" variant="contained" onClick={loginWithGoogle}>
+          Login
+        </Button>
+        </Paper>
+        </>
+    )
+  }
+
   return (
     <>
       {user ? (
         <Button color="primary" variant="contained" onClick={logout}>
           Logout
         </Button>
-      ) : (
-        <Button color="primary" variant="contained" onClick={loginWithGoogle}>
-          Login
-        </Button>
-      )}
+      ) : loginPage()}
     </>
   );
 };
