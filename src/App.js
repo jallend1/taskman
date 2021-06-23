4d3d58467248c219779ea5755a10a895e4f8a2b5
jallend1 <jallend1@gmail.com>
2021-06-23 14:32:30 -0700

    Removes version 1 components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
 import ProjectContextProvider from './Contexts/ProjectContext';
 import AuthContextProvider from './Contexts/AuthContext';
-
 import NavBar from './Components/NavBar';
 import Home from './Components/Home';
 import About from './Components/About';
-import V1 from './v1/Components/Home';
 import NewProject from './Components/NewProject';
 import Project from './Components/Project';
 import Login from './Components/Login';
@@ -22,24 +19,21 @@ function App() {
             <Route path="/about">
               <About />
             </Route>
-            <Route path="/v1">
-              <V1 />
-            </Route>
             <ProjectContextProvider>
+              <Route path="/login">
+                <Login />
+              </Route>
               <Route path="/new">
                 <NewProject />
               </Route>
-              <Route exact path="/">
-                <Home />
+              <Route path="/profile">
+                <Profile />
               </Route>
               <Route path="/project/:id">
                 <Project projectInURL={true} />
               </Route>
-              <Route path="/login">
-                <Login />
-              </Route>
-              <Route path="/profile">
-                <Profile />
+              <Route exact path="/">
+                <Home />
               </Route>
             </ProjectContextProvider>
           </Switch>
